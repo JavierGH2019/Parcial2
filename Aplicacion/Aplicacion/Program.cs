3c27c8a8ec2e777c93cf3375ad9c95c41325b2ea
JavierGH2019 <47840404+JavierGH2019@users.noreply.github.com>
2019-03-19 19:33:22 -0600

    Add files via upload

---
+++ Aplicacion/Aplicacion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace Aplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicios Obj = new Ejercicios();
+            Console.WriteLine("Selecciones ejercicio a calificar");
+            Console.WriteLine("1----Ejercicio 1");
+            Console.WriteLine("2----Ejercicio 2");
+            Console.WriteLine("3----Ejercicio 3");
+            Console.WriteLine("4----Ejercicio 4");
+            int n = int.Parse(Console.ReadLine());
+
+            if(n==1){
+                Obj.Ejercicio1();
+                Console.ReadKey();
+            }
+            else if (n == 2)
+            {
+                Obj.Ejercicio2();
+                Console.ReadKey();
+            }
+            else if (n == 3)
+            {
+                Obj.Ejercicio3();
+                Console.ReadKey();
+            }
+            else if (n == 4)
+            {
+                Obj.Ejercicio4();
+                Console.ReadKey();
+            }
+            else  
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ Aplicacion/ClassLibrary1/Ejercicios.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Ejercicios
+    {
+        public void Ejercicio1(){
+            int n1 = 0,n2=0;
+
+            Console.WriteLine("Por favor ingrese el primer numero");
+              n1 = int.Parse(Console.ReadLine());
+
+            if(n1==0){
+                Console.WriteLine("El producto de 0 por cualquier numero es 0");               
+            }
+            else
+            {
+                Console.WriteLine("Por favor digite otro numero. ");
+                  n2 = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("El producto de "+n1+"*"+n2+"es=: "+(n1*n2));
+    }
+        public void Ejercicio2() {
+            int n = 0,n1=0,n2=0;
+            Console.WriteLine("Digite 2");
+           n=int.Parse(Console.ReadLine());
+            if(n==2){
+                Console.WriteLine("Digite un primer numero ");
+                n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite un segundo numero ");
+                n2 = int.Parse(Console.ReadLine());
+
+                if (n2 == 0)
+                {
+                    Console.WriteLine("error no se puede dividir entre 0");
+                }
+                else {
+                    Console.WriteLine("Resultado "+(n1/n2));
+                }
+            }
+        
+        }
+        public void Ejercicio3() {
+            int n = 0;
+            Console.WriteLine("Presione3");
+            n = int.Parse(Console.ReadLine());
+
+            if(n==3){
+                int i=0;
+                 
+                string[] nombre=new string[5];
+                for (i = 0; i < 5;i++ )
+                {
+                    Console.WriteLine("Digite nombre"+(i+1));
+                    nombre[i] = Console.ReadLine();
+                    
+                }
+                for (i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Hola-----"+nombre[i]);                  
+
+                }
+
+
+            }
+        }
+        public void Ejercicio4() {
+            int n,i=1,i2=0;
+            string[] edad=new string[5];
+            Console.WriteLine("Ingrese numero 4");
+            n = int.Parse(Console.ReadLine());
+
+            if(n==4){
+                do
+                {
+                    Console.WriteLine("Digitela edad del alumno: " + i);
+                    edad[i - 1] = Console.ReadLine();
+                    i++;
+                }
+                while (i < 6);
+                do
+                {
+                    Console.WriteLine("Edad-"+(i2+1)+"=" + edad[i2]+"años");
+                    i2++;
+                     
+                }
+                while (i2 < 5);
+
+            }
+
+        }
+         
+
+    }
+}
